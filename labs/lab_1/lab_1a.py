a299b0f97e994171001da325c0f9d21f51f21759
aic15902020-ctrl <aic15902020@gmail.com>
2026-07-05 19:36:06 -0400

    Add description to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -9,8 +9,10 @@ def main():
     print("Hello World!")
 
     name = "aidan chen" # TODO: Insert your name between the double quotes
+    desc = "Hello! My name is Aidan Chen, and I'm a sophomore at the Academy for Math, Science, and Engineering down (or up...) here in New Jersey. I am a drummer and have been in concert bands, jazz bands, and lots else for...too many years to count. I also have an interest in STEM and have been in a robotics team ever since I started back in 7th grade. I look forward to collaborating with everyone here as we move forward in this course!"
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Description: " + desc)
 
 if __name__ == "__main__":
     main()
